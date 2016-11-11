1412b5c90e035e4323887cf3cdf1514c8acb2e06
sabrym <sabry.moulana@gmail.com>
2016-11-11 11:52:34 +0530

    master : Initial commit

---
+++ tasks/react_dependency_fixer.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-react-dependency-fixer
+ * https://github.com/sabrym/grunt-react-dependency-fixer
+ *
+ * Copyright (c) 2016 sabrym
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('react_dependency_fixer', 'This is a plugin that checks for requireJs dependencies in javascript files and updates them if their case does not match the defined', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
